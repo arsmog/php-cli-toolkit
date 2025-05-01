e197dd377733d576236fd3701a15a8c7d333e535
Arseny Mogilev <arsmog@yahoo.com>
2025-05-01 22:45:36 +0300

    Create autoload

---
+++ autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+spl_autoload_register(function ($class_name) {
+    $class_file = str_replace(
+        ["//", "\\"],
+        "/",
+        sprintf("%s/%s/%s", __DIR__,  "src", $class_name)
+    );
+    if (file_exists($class_file)) {
+        include_once $class_file;
+    }
+});
