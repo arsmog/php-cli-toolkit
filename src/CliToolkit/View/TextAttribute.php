1803d4138c7a52c35b6c8d3c09d9bf7e449487ad
Arseny Mogilev <arsmog@yahoo.com>
2025-05-02 23:31:23 +0300

    Added strikethrough text

--- src/CliToolkit/View/TextAttribute.php
+++ src/CliToolkit/View/TextAttribute.php
@@ -4,12 +4,13 @@ namespace CliToolkit\View;
 
 class TextAttribute
 {
-    public const RESET = 0;     // Reset all attributes
-    public const BOLD = 1;      // Bold text
-    public const FADED = 2;     // Faded text
-    public const ITALIC = 3;    // Italics
-    public const UNDERLINE = 4; // Underlined text
-    public const BLINK = 5;     // Flashing text
-    public const INVERTED = 7;  // Inverted color
-    public const HIDDEN = 8;    // Скрытый текст
+    public const RESET = 0;               // Reset all attributes
+    public const BOLD = 1;                // Bold text
+    public const FADED = 2;               // Faded text
+    public const ITALIC = 3;              // Italics
+    public const UNDERLINE = 4;           // Underlined text
+    public const BLINK = 5;               // Flashing text
+    public const INVERTED = 7;            // Inverted color
+    public const HIDDEN = 8;              // Hidden text
+    public const STRIKETHROUGH = 9;       // Strikethrough text
 }
